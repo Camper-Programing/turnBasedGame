f8879df400295c609c6081669da9f42dd92a1133
R j <tangjacobsen@gmail.com>
2025-10-29 10:49:52 +0100

    First version setup

--- turnBasedGame/world/world.cs
+++ turnBasedGame/world/world.cs
@@ -1,7 +1,20 @@
-﻿namespace turnBasedGame.world
+﻿using turnBasedGame.model.creature;
+using turnBasedGame.world;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace turnBasedGame.world
 {
     public class world
     {
+        public int maxX { get; set; }
+        public int maxY { get; set; }
+
+        private readonly List<worldObject> worldObjects;
+        private readonly List<creature> creature;
+
 
     }
 }
