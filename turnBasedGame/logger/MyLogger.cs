821c6cdce059aeee6d633916e1842909a44f598b
R j <tangjacobsen@gmail.com>
2025-10-29 13:05:14 +0100

    Add of logger

---
+++ turnBasedGame/logger/MyLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using ILoggerFactory
+
+namespace turnBasedGame.logger
+{
+    public sealed class MyLogger
+    {
+    }
+}
