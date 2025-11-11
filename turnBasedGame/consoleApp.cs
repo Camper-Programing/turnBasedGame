8ddfc9ab754c5837e549b9286854f7da3b09acb0
R J <tangjacobsen@gmail.com>
2025-11-11 03:14:22 +0100

    This might need it

--- turnBasedGame/consoleApp.cs
+++ turnBasedGame/consoleApp.cs
@@ -4,11 +4,13 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using turnBasedGame.Config;
 using turnBasedGame.logger;
-using turnBasedGame.model.Creature;
-using turnBasedGame.World;
 using turnBasedGame.model.Attack;
+using turnBasedGame.model.Creature;
 using turnBasedGame.model.Defense;
+using turnBasedGame.Factory;
+using turnBasedGame.World;
 
 
 
@@ -18,11 +20,18 @@ namespace turnBasedGame //test class ´for the framework
     {
         static void Main(string[] args)
         {
+            Console.WriteLine($"Current directory: {Environment.CurrentDirectory}"); //to help debug file path issues
             //Test of logging
             MyLogger.Instance.addListener(new ConsoleTraceListener());
 
             //create world
-            var World = new World.World(10, 10, MyLogger.Instance);
+            /*var World = new World.World(10, 10, MyLogger.Instance); the old way when the game created a world*/
+
+            string configPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Config.xml");
+            var (maxX, maxY, difficulty) = ConfigLoader.LoadConfig(configPath); //if it does not work use a hardcodeded path like @"C:\path\to\your\Config.xml"
+            MyLogger.Instance.Log($"Game configuration loaded: maxX={maxX}, maxY={maxY}, difficulty={difficulty}");
+
+            var World = new World.World(maxX, maxY, MyLogger.Instance); //new way where the world is created based on the config file
 
             //create Creature
             /*var Creature = new Creature(2, 3, 100);*/
@@ -30,10 +39,22 @@ namespace turnBasedGame //test class ´for the framework
 
             var hero = new Hero(1, 1, 150);
             var enemy = new Enemy(5, 5, 80);
+            
+            /*hero.AddAttackItem(new Sword());*/
+
+            hero.AddAttackItem(GameObjectFactory.CreateAttackItem("Sword"));
+            //Create Defense Items using factory.
+            enemy.AddDefenseItem(GameObjectFactory.CreateDefenseItem("Shield"));
+
+            var shield = new Shield();
+            var boostedSheild = new DefenseDecorator(shield, 5); //should add 10 points of damage protection
+            var compositeDefense = new DefenseComposite();
+            compositeDefense.Add(shield);
+            compositeDefense.Add(boostedSheild); //This is to test the decorator and composite pattern
 
             //Add Items to Creature
-            hero.AddAttackItem(new Sword());
-            enemy.AddDefenseItem(new Shield());
+
+            enemy.AddDefenseItem(compositeDefense);
 
             //Add Creature to World
             World.AddCreature(hero);
@@ -42,6 +63,7 @@ namespace turnBasedGame //test class ´for the framework
             //Test Attack Hero to Enemy
             hero.Hit(enemy);
             MyLogger.Instance.Log($"Enemy HP after hit: {enemy.HP}");
+            Console.WriteLine($"Enemy HP after attack: {enemy.HP}");
 
             enemy.Hit(hero);
             MyLogger.Instance.Log($"Hero HP after hit: {hero.HP}");
@@ -49,10 +71,9 @@ namespace turnBasedGame //test class ´for the framework
 
             //Move Creature
 
-            hero.MoveTo(4, 5);
+            /*hero.MoveTo(4, 5);
             MyLogger.Instance.Log($"Creature moved to ({hero.PositionX}, {hero.PositionY})"); /* The first test has been concluded, had to change a few things here and there in order for it to work, one was mainly naming problems with the program*/
-
-            Console.WriteLine("Press any key to exit...check the log for details.");
+            //Console.WriteLine("Press any key to exit...check the log for details.");*/
             Console.ReadKey();
         }
 
