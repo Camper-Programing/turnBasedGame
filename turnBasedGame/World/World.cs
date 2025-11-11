eaad780d582ecd501ab04366b121a2e25916d29a
R J <tangjacobsen@gmail.com>
2025-11-11 05:45:53 +0100

    Final thing

--- turnBasedGame/World/World.cs
+++ turnBasedGame/World/World.cs
@@ -10,7 +10,17 @@ namespace turnBasedGame.World
     /// </summary>
     public class World
     {
+        /// <summary>
+        /// This is to log the events of the world
+        /// </summary>
+        /// <remarks> used internally for logging use.</remarks>
         private readonly Ilogger _logger;
+
+        /// <summary>
+        /// Initializes a new instance of the World class with specified dimensions.
+        /// </summary>
+        /// <param name="maxX">The maximum X-coordinate of the world.</param>
+        /// <param name="maxY">The maximum Y-coordinate of the world.</param>
 
         public int MaxX { get; }
         public int MaxY { get; }
@@ -26,18 +36,30 @@ namespace turnBasedGame.World
             _logger.Log($"World created with size {MaxX}x{MaxY}");
         }
 
+        /// <summary>
+        /// adds a world object to the world
+        /// </summary>
+        /// <param name="obj"></param>
         public void AddWorldObject(WorldObject obj)
         {
             _worldObjects.Add(obj);
             _logger.Log($"World object added at position ({obj.positionX}, {obj.positionY})");
         }
-
+        /// <summary>
+        /// adds a creature to the world
+        /// </summary>
+        /// <param name="creature"></param>
         public void AddCreature(Creature creature)
         {
             _creatures.Add(creature);
             _logger.Log($"Creature added at position ({creature.PositionX}, {creature.PositionY})");
         }
-
+        /// <summary>
+        /// to check it a postion on the map/world is occupied
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
         public bool IsOccupied(int x, int y) =>
             _creatures.Any(c => c.PositionX == x && c.PositionY == y) ||
             _worldObjects.Any(o => o.positionX == x && o.positionY == y);
