2ea8dd2763e88b308837ca142d94376525bc938c
R J <tangjacobsen@gmail.com>
2025-11-11 05:31:44 +0100

    Code cleanup with the use of visual studio.

--- turnBasedGame/World/World.cs
+++ turnBasedGame/World/World.cs
@@ -1,41 +1,45 @@
-﻿using turnBasedGame.model.Creature;
+﻿using System.Collections.Generic;
+using System.Linq;
+using turnBasedGame.model.Creature;
+using turnBasedGame.logger;
 
 namespace turnBasedGame.World
 {
-    internal class World
+    /// <summary>
+    /// Represents the world grid and manages creatures and world objects.
+    /// </summary>
+    public class World
     {
-        private readonly Ilogger logger;
-        public int maxX { get; set; }
-        public int maxY { get; set; }
+        private readonly Ilogger _logger;
 
-        private readonly List<WorldObject> WorldObjects = new ();
-        private readonly List<Creature> Creature = new ();
+        public int MaxX { get; }
+        public int MaxY { get; }
+
+        private readonly List<WorldObject> _worldObjects = new();
+        private readonly List<Creature> _creatures = new();
 
         public World(int maxX, int maxY, Ilogger logger)
         {
-            this.maxX = maxX;
-            this.maxY = maxY;
-            this.logger = logger;
-            logger.Log($"World created with size {maxX}x{maxY}");
-
+            MaxX = maxX;
+            MaxY = maxY;
+            _logger = logger;
+            _logger.Log($"World created with size {MaxX}x{MaxY}");
         }
+
         public void AddWorldObject(WorldObject obj)
         {
-            WorldObjects.Add(obj);
-            logger.Log($"World object added at position ({obj.positionX}, {obj.positionY})");
+            _worldObjects.Add(obj);
+            _logger.Log($"World object added at position ({obj.positionX}, {obj.positionY})");
         }
-        public void AddCreature(Creature Creature)
+
+        public void AddCreature(Creature creature)
         {
-            this.Creature.Add(Creature);
-            logger.Log($"Creature added at position ({Creature.PositionX}, {Creature.PositionY})");
+            _creatures.Add(creature);
+            _logger.Log($"Creature added at position ({creature.PositionX}, {creature.PositionY})");
         }
 
-        public bool IsOccupied(int x, int y)
-        {
-            return Creature.Any(c => c.PositionX == x && c.PositionY == y) ||
-                   WorldObjects.Any(o => o.positionX == x && o.positionY == y);
-        }
-
-
+        public bool IsOccupied(int x, int y) =>
+            _creatures.Any(c => c.PositionX == x && c.PositionY == y) ||
+            _worldObjects.Any(o => o.positionX == x && o.positionY == y);
     }
 }
--- turnBasedGame/world/worldObject.cs
+++ turnBasedGame/world/worldObject.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace turnBasedGame.World
 {
-    internal class WorldObject
+    public class WorldObject
     {
-        public int positionX {  get; private set; }
+        public int positionX { get; private set; }
         public int positionY { get; private set; }
-        public bool IsRemovalbe {  get; private set; }
+        public bool IsRemovalbe { get; private set; }
 
         public WorldObject(int x, int y, bool isRemovalbe = true)
         {
